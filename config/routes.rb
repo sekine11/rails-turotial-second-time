5371520742c8d7c8b103679ca15205a131c22cff
EC2 Default User <ec2-user@ip-172-31-80-249.ec2.internal>
2020-03-18 10:44:56 +0000

    [Finish] rails-tutorial-6-second-time

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get 'users/new'
+
+  get  '/', to: 'static_pages#home'
+
+  get  '/about', to: 'static_pages#about'
+  
+  get  '/contact', to: 'static_pages#contact'
+  
+  get  '/help', to: 'static_pages#help'
+  
+  get  '/signup', to: 'users#new'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root "static_pages#home"
+end
