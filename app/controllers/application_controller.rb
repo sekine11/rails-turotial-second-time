8a35ae33bd0cddf0c2717948eaf017421b030b31
EC2 Default User <ec2-user@ip-172-31-80-249.ec2.internal>
2020-03-19 09:31:50 +0000

    [Finish] rails-tutorial-8-socond-time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def hi
-    render html: "hi"
-  end
+  include SessionsHelper
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,11 +3,14 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
+include ApplicationHelper
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  include ApplicationHelper
-
+  # テストユーザーがログイン中の場合にtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
   # Add more helper methods to be used by all tests here...
 end
